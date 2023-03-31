7480eb86454a71c33eb97e4ffa779e6407e5055c
Lakshay Dang <lakshayisdang@gmail.com>
2023-03-30 21:57:23 -0700

    Milestone 4 completed for Person 3

---
+++ analysis/project_functions3.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def importLibraries():
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    
+def load_and_process(path):
+    data = pd.read_csv(path)
+
+    ''' Processing Dataset
+        -Removing unused columns (Hispanic, Police)
+        -Removing any non-suicide records
+        -Resetting Index
+        -Removing the 'index' column that we don't need
+        -Dropping all NaN values from the dataset
+    '''
+    df_cleaned = (data.drop(columns = [data.columns[0], 'hispanic', 'police'])
+              .loc[data.intent == "Suicide"]
+              .reset_index()
+              .drop(columns = 'index')
+              .dropna()
+             )
+
+    return df_cleaned
